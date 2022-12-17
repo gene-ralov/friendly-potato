81a8072d9cc63715277a5044a1fa4d0e08059a44
Nikolai Generalov <gene-ralov@ya.ru>
2022-12-17 23:02:46 +0400

    Добавил task005, task006

---
+++ HomeWork/task006/Program.cs
@@ -0,0 +1,8 @@
+﻿// 6. Показать вторую цифру трёхзначного числа
+
+int Number = new Random().Next(100, 999);
+int a = Number / 10;
+int b = a % 10;
+
+Console.WriteLine("Заданное число: " + Number);
+Console.WriteLine("Вторая цифра числа: " + b);
