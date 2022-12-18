3689c2d3ae73fc2fb8a177d81870ef4e25c223e4
Nikolai Generalov <gene-ralov@ya.ru>
2022-12-18 13:50:56 +0400

    Две задачи

--- HomeWork/task010/Program.cs
+++ HomeWork/task010/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число.\nДля выхода из программы введите Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
