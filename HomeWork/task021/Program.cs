abd3188a253645f36eec23a6c839a2f9c1a5f24d
Nikolai Generalov <gene-ralov@ya.ru>
2022-12-25 13:29:00 +0400

    ДЗ за 5 и 6 урок

---
+++ HomeWork/task021/Program.cs
@@ -0,0 +1,16 @@
+﻿// 21. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+var array = new decimal[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Введите {0} число : ", i+1);
+                array[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+ 
+            var max = array[0];
+            var min = array[0];
+            for (int i = 1; i < 10; i++)
+            {
+                if (array[i] < min) min = array[i];
+                if (array[i] > max) max = array[i];
+            }
+            Console.WriteLine("Разность между максимальным и минимальным = {0}", max - min);
---
+++ HomeWork/task025/Program.cs
@@ -0,0 +1,6 @@
+﻿// 25. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine($"В двоичном будет: {binary}");
+            Console.ReadLine();
